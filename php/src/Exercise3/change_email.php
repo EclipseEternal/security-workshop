93968343d9326a6e2a4092b7bb631b2fc0472c82
herve.mouille <herve.mouille@smartbox.com>
2024-06-13 10:12:17 +0100

    Fix exercise 3

--- php/src/Exercise3/change_email.php
+++ php/src/Exercise3/change_email.php
@@ -5,12 +5,12 @@ $conn = new mysqli($_ENV['MYSQL_HOST'], $_ENV['MYSQL_USER'], $_ENV['MYSQL_PASSWO
 echo sprintf('Logged in user ID: %d \n', $_SESSION['user_id']);
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $new_email = $_POST['email'];
+    $new_user_name = $_POST['username'];
     $user_id = $_SESSION['user_id']; // Assuming the user is logged in and user_id is stored in session
 
-    $query = "UPDATE users SET email = '$new_email' WHERE id = $user_id";
+    $query = "UPDATE users SET `username` = '$new_user_name' WHERE id = $user_id";
     if ($conn->query($query)) {
-        echo "Email changed successfully.";
+        echo "User name changed successfully.";
     } else {
         echo "Failed to change email.";
     }
--- php/src/Exercise3/form.php
+++ php/src/Exercise3/form.php
@@ -1,3 +1,6 @@
+<?php
+    session_start();
+    ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,16 +9,16 @@
 </head>
 <body>
 <?php
-session_start();
+
 $_SESSION['user_id'] = 1;
-echo sprintf('Logged in user ID: %d \n', $_SESSION['user_id']);
+echo sprintf('Logged in user ID: %d <br/>', $_SESSION['user_id']);
 
 ?>
-<h1>Change User Email</h1>
+<h1>Change User Name</h1>
 <form method="post" action="change_email.php">
-    <label for="email">New Email:</label>
-    <input type="email" id="email" name="email" required>
-    <button type="submit">Change Email</button>
+    <label for="username">New User name:</label>
+    <input id="username" name="username" required>
+    <button type="submit">Change Name</button>
 </form>
 </body>
 </html>
